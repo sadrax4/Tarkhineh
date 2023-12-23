cf4e2ef4701b36d66ea3dc97ad66d21ba552ba72
sadra soleimani <sadrasadrasadra6@gmail.com>
2023-12-23 20:21:49 +0330

    start project

---
+++ src/app.module.ts
@@ -0,0 +1,21 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>("MONGODB_URI")
+      })
+    })
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule { }
