5b475d1e6611e3e28ad3c571e80060c321c759f0
sadra soleimani <sadrasadrasadra6@gmail.com>
2023-12-26 15:39:33 +0330

    fix resend code

--- src/auth/dto/resend-code-dto.ts
+++ src/auth/dto/resend-code-dto.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { IsNotEmpty, IsPhoneNumber } from "class-validator";
 
 const errorMessage = "شماره تلفن وارد شده صحیح نمیباشد";
@@ -5,5 +6,11 @@ export class ResendCodeDto {
 
     @IsNotEmpty({ message: errorMessage })
     @IsPhoneNumber('IR', { message: errorMessage })
+    @ApiProperty({
+        title: "enter phone number for login",
+        example: "09123456789",
+        nullable: false
+    })
     phone:string
+
 }
