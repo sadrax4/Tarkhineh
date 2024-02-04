2e2540682471ae7c1c47bfd220d9d313e94d160f
sadra soleimani <sadrasadrasadra6@gmail.com>
2024-02-04 23:02:13 +0330

    build: admin get comments list

--- src/admin/admin-comment/admin-comment.controller.ts
+++ src/admin/admin-comment/admin-comment.controller.ts
@@ -1,4 +1,45 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Delete, Get, HttpStatus, Param, Res, UseGuards } from '@nestjs/common';
+import { ApiResponse, ApiTags } from '@nestjs/swagger';
+import { Response } from 'express';
+import { JwtGuard } from 'src/auth/guards';
+import { CommentService } from 'src/comment/comment.service';
+import { OkResponseMessage } from 'src/common/constant';
 
-@Controller('admin-comment')
-export class AdminCommentController {}
+@Controller('admin')
+export class AdminCommentController {
+    constructor(
+        private readonly commentService: CommentService
+    ) { }
+
+    @UseGuards(JwtGuard)
+    @ApiTags('admin-comment')
+    @ApiResponse({
+        type: OkResponseMessage,
+        status: HttpStatus.OK
+    })
+    @Get("comments")
+    async getComments(
+        @Res() response: Response
+    ): Promise<Response> {
+        return this.commentService.getComments(
+            response
+        );
+    }
+
+    @UseGuards(JwtGuard)
+    @ApiTags('admin-comment')
+    @ApiResponse({
+        type: OkResponseMessage,
+        status: HttpStatus.OK
+    })
+    @Delete("comments/:id")
+    async deleteComment(
+        @Res() response: Response,
+        @Param("id") commentId: string
+    ): Promise<Response> {
+        return this.commentService.deleteComment(
+            commentId,
+            response
+        );
+    }
+}
--- src/admin/admin-comment/admin-comment.module.ts
+++ src/admin/admin-comment/admin-comment.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AdminCommentService } from './admin-comment.service';
 import { AdminCommentController } from './admin-comment.controller';
+import { CommentModule } from 'src/comment/comment.module';
 
 @Module({
+  imports: [
+    CommentModule
+  ],
   providers: [AdminCommentService],
   controllers: [AdminCommentController]
 })
-export class AdminCommentModule {}
+export class AdminCommentModule { }
