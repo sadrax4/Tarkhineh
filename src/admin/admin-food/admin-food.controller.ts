900f7abae1aa1586c527b4ec1deffd2954d9e54c
sadra soleimani <sadrasadrasadra6@gmail.com>
2024-02-04 12:46:10 +0330

    fix: connect to mongo via proxy

--- src/admin/admin-food/admin-food.controller.ts
+++ src/admin/admin-food/admin-food.controller.ts
@@ -1,14 +1,21 @@
-import { Controller, Get, HttpStatus, Res, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, HttpStatus, Post, Res, UploadedFiles, UseGuards, UseInterceptors } from '@nestjs/common';
 import { AdminFoodService } from './admin-food.service';
-import { OkResponseMessage } from 'src/common/constant';
+import { MIMETYPE, OkResponseMessage } from 'src/common/constant';
 import { JwtGuard } from 'src/auth/guards';
-import { ApiResponse, ApiTags } from '@nestjs/swagger';
+import { ApiBody, ApiConsumes, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { Response } from 'express';
+import { StringToArray } from 'src/common/decorators';
+import { MulterFile } from 'src/common/types';
+import { CreateFoodDto } from 'src/food/dto';
+import { FoodService } from 'src/food/food.service';
+import { foodSchema } from 'src/food/config';
+import { UploadMultiFilesAws } from 'src/common/interceptors';
 
 @Controller('admin')
 export class AdminFoodController {
     constructor(
-        private adminFoodService: AdminFoodService
+        private adminFoodService: AdminFoodService,
+        private foodService: FoodService
     ) { }
 
     @UseGuards(JwtGuard)
@@ -25,4 +32,26 @@ export class AdminFoodController {
             response
         );
     }
+
+    // @UseGuards(JwtGuard)
+    // @ApiBody(foodSchema)
+    // @UseInterceptors(UploadMultiFilesAws('images'))
+    // @ApiTags('admin-food')
+    // @ApiConsumes(MIMETYPE.MULTIPART)
+    // @ApiResponse({
+    //     type: OkResponseMessage,
+    //     status: HttpStatus.CREATED
+    // })
+    // @Post("foods")
+    // async createFood(
+    //     @StringToArray("ingredients") _: null,
+    //     @UploadedFiles() images: Array<MulterFile>,
+    //     @Body() createFoodDto: CreateFoodDto,
+    //     @Res() response: Response
+    // ): Promise<Response> {
+    //     return this.foodService.createFood(
+    //         CreateFoodDto
+    //         response
+    //     );
+    // }
 }
