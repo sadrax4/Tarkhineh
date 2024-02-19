ddd30103050224eed5cedfc490f816a597e9efd4
sadra soleimani <sadrasadrasadra6@gmail.com>
2024-02-19 14:26:46 +0330

    build: set order status  admin-controller

--- src/admin/admin-order/admin-order.controller.ts
+++ src/admin/admin-order/admin-order.controller.ts
@@ -1,9 +1,10 @@
 import { OkResponseMessage, UnAuthorizeResponseMessage } from '@app/common';
-import { Controller, Get, HttpStatus, Post, Query, Res, UseGuards } from '@nestjs/common';
-import { ApiOperation, ApiResponse, ApiTags, ApiUnauthorizedResponse } from '@nestjs/swagger';
+import { Body, Controller, Get, HttpStatus, Post, Query, Res, UseGuards } from '@nestjs/common';
+import { ApiBody, ApiOperation, ApiResponse, ApiTags, ApiUnauthorizedResponse } from '@nestjs/swagger';
 import { AdminGuard } from 'src/auth/guards';
 import { AdminOrderService } from './admin-order.service';
 import { Response } from 'express';
+import { OrderStatusDto } from './dto';
 
 @Controller('admin')
 export class AdminOrderController {
@@ -34,6 +35,7 @@ export class AdminOrderController {
     @UseGuards(AdminGuard)
     @ApiOperation({ summary: "set order status " })
     @ApiTags('admin-order')
+    @ApiBody({ type: OrderStatusDto })
     @ApiUnauthorizedResponse({
         type: UnAuthorizeResponseMessage,
         status: HttpStatus.UNAUTHORIZED
@@ -44,11 +46,13 @@ export class AdminOrderController {
     })
     @Post("order")
     async setOrderStatus(
+        @Body() orderStatusDto: OrderStatusDto
         @Query("id") orderId: string,
         @Res() response: Response
     ): Promise<Response> {
         return this.adminOrderService.setOrderStatus(
             orderId,
+            orderStatusDto
             response
         )
     }
