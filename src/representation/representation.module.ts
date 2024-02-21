e39244ebdc0e335c88eb165dd1e6153d4998a32b
sadra soleimani <sadrasadrasadra6@gmail.com>
2024-02-21 11:12:30 +0330

    build: mongo representation schema

--- src/representation/representation.module.ts
+++ src/representation/representation.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { RepresentationService } from './representation.service';
 import { RepresentationController } from './representation.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Representation, RepresentationSchema } from './db/representation.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: Representation.name,
+        schema: RepresentationSchema
+      }
+    ])
+  ],
   providers: [RepresentationService],
   controllers: [RepresentationController]
 })
-export class RepresentationModule {}
+export class RepresentationModule { }
