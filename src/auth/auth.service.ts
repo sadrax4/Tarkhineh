be43f41be6c493c090d43115ff36f5b6cd2422f5
sadra soleimani <sadrasadrasadra6@gmail.com>
2024-04-24 12:54:56 +0330

    refactor: try catch handler <auth service>

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -42,10 +42,14 @@ export class AuthService {
                     statusCode: HttpStatus.OK
                 })
         } catch (error) {
-            throw new HttpException(
-                (INTERNAL_SERVER_ERROR_MESSAGE + error),
-                HttpStatus.INTERNAL_SERVER_ERROR
-            )
+            if (error instanceof HttpException) {
+                throw error;
+            } else {
+                throw new HttpException(
+                    (error),
+                    HttpStatus.INTERNAL_SERVER_ERROR
+                );
+            }
         }
     }
 
@@ -54,51 +58,62 @@ export class AuthService {
         otpCode: number,
         response: Response
     ): Promise<Response> {
-        const user = await this.userService.findUser(phone);
-        const {
-            expireIn: otpExpireIn,
-            code: userCode
-        } = user.otp;
-        const now: number = new Date().getTime();
-        if (now > otpExpireIn) {
-            throw new HttpException(
-                "کد وارد شده منقضی شده است",
-                HttpStatus.UNAUTHORIZED
-            )
+        try {
+            const user = await this.userService.findUser(phone);
+            const {
+                expireIn: otpExpireIn,
+                code: userCode
+            } = user.otp;
+            const now: number = new Date().getTime();
+            if (now > otpExpireIn) {
+                throw new HttpException(
+                    "کد وارد شده منقضی شده است",
+                    HttpStatus.UNAUTHORIZED
+                )
+            }
+            if (userCode !== otpCode) {
+                throw new HttpException(
+                    "کد وارد شده اشتباه است",
+                    HttpStatus.UNAUTHORIZED
+                )
+            }
+            const tokens: Token = await this.getTokens(
+                user.phone,
+                user.username
+            );
+            const hashRT = await bcrypt.hash(
+                tokens.refreshToken,
+                10
+            );
+            await this.userService.saveRefreshToken(phone, hashRT);
+            response.cookie(
+                'access-token',
+                tokens.accessToken,
+                AccessCookieConfig
+            );
+            response.cookie(
+                "refresh-token",
+                tokens.refreshToken,
+                RefreshCookieConfig
+            );
+            const successMessage = "ورود با موفقیت انجام شد";
+            return response
+                .status(HttpStatus.OK)
+                .json({
+                    tokens,
+                    message: successMessage,
+                    statusCode: HttpStatus.OK
+                })
+        } catch (error) {
+            if (error instanceof HttpException) {
+                throw error;
+            } else {
+                throw new HttpException(
+                    (error),
+                    HttpStatus.INTERNAL_SERVER_ERROR
+                );
+            }
         }
-        if (userCode !== otpCode) {
-            throw new HttpException(
-                "کد وارد شده اشتباه است",
-                HttpStatus.UNAUTHORIZED
-            )
-        }
-        const tokens: Token = await this.getTokens(
-            user.phone,
-            user.username
-        );
-        const hashRT = await bcrypt.hash(
-            tokens.refreshToken,
-            10
-        );
-        await this.userService.saveRefreshToken(phone, hashRT);
-        response.cookie(
-            'access-token',
-            tokens.accessToken,
-            AccessCookieConfig
-        );
-        response.cookie(
-            "refresh-token",
-            tokens.refreshToken,
-            RefreshCookieConfig
-        );
-        const successMessage = "ورود با موفقیت انجام شد";
-        return response
-            .status(HttpStatus.OK)
-            .json({
-                tokens,
-                message: successMessage,
-                statusCode: HttpStatus.OK
-            })
     }
 
     async refreshToken(
@@ -106,150 +121,194 @@ export class AuthService {
         refreshToken: string,
         userPhone: string
     ): Promise<Response | void> {
-        const {
-            hashRT,
-            phone,
-            username
-        } = await this.userService.findUser(userPhone);
-        const isTokensEqual: boolean = await bcrypt.compare(
-            refreshToken,
-            hashRT
-        );
-        if (!isTokensEqual) {
-            throw new HttpException(
-                "توکن نا معتبر ",
-                HttpStatus.UNAUTHORIZED
+        try {
+            const {
+                hashRT,
+                phone,
+                username
+            } = await this.userService.findUser(userPhone);
+            const isTokensEqual: boolean = await bcrypt.compare(
+                refreshToken,
+                hashRT
             );
-        }
-        const isValidToken = await this.jwtService.verify(
-            refreshToken,
-            {
-                secret: this.configService.get<string>("JWT_REFRESH_TOKEN_SECRET")
+            if (!isTokensEqual) {
+                throw new HttpException(
+                    "توکن نا معتبر ",
+                    HttpStatus.UNAUTHORIZED
+                );
             }
-        )
-        if (!isValidToken) {
-            throw new HttpException(
-                "توکن نا معتبر ",
-                HttpStatus.UNAUTHORIZED
+            const isValidToken = await this.jwtService.verify(
+                refreshToken,
+                {
+                    secret: this.configService.get<string>("JWT_REFRESH_TOKEN_SECRET")
+                }
+            )
+            if (!isValidToken) {
+                throw new HttpException(
+                    "توکن نا معتبر ",
+                    HttpStatus.UNAUTHORIZED
+                );
+            }
+            const tokens: Token = await this.getTokens(
+                phone,
+                username
             );
+            const hashRefresh = await bcrypt.hash(
+                tokens.refreshToken,
+                10
+            );
+            await this.userService.saveRefreshToken(
+                phone,
+                hashRefresh
+            );
+            response.cookie(
+                'access-token',
+                tokens.accessToken,
+                AccessCookieConfig
+            );
+            response.cookie(
+                "refresh-token",
+                tokens.refreshToken,
+                RefreshCookieConfig
+            );
+            const responseMessage = 'توکن جدید تولید شد'
+            return response
+                .status(HttpStatus.OK)
+                .json({
+                    message: responseMessage,
+                    statusCode: HttpStatus.OK
+                })
+        } catch (error) {
+            if (error instanceof HttpException) {
+                throw error;
+            } else {
+                throw new HttpException(
+                    (error),
+                    HttpStatus.INTERNAL_SERVER_ERROR
+                );
+            }
         }
-        const tokens: Token = await this.getTokens(
-            phone,
-            username
-        );
-        const hashRefresh = await bcrypt.hash(
-            tokens.refreshToken,
-            10
-        );
-        await this.userService.saveRefreshToken(
-            phone,
-            hashRefresh
-        );
-        response.cookie(
-            'access-token',
-            tokens.accessToken,
-            AccessCookieConfig
-        );
-        response.cookie(
-            "refresh-token",
-            tokens.refreshToken,
-            RefreshCookieConfig
-        );
-        const responseMessage = 'توکن جدید تولید شد'
-        return response
-            .status(HttpStatus.OK)
-            .json({
-                message: responseMessage,
-                statusCode: HttpStatus.OK
-            })
     }
 
     async logout(
         phone: string,
         response: Response
-    ) {
-        const d = new Date();
-        response.clearCookie(
-            "refresh-token",
-            {
-                sameSite: 'none',
-                httpOnly: false,
-                secure: true
+    ): Promise<Response> {
+        try {
+            const d = new Date();
+            response.clearCookie(
+                "refresh-token",
+                {
+                    sameSite: 'none',
+                    httpOnly: false,
+                    secure: true
+                }
+            );
+            response.clearCookie(
+                "access-token",
+                {
+                    sameSite: 'none',
+                    httpOnly: false,
+                    secure: true
+                }
+            );
+            delete response?.req?.user;
+            delete response?.req?.cookies;
+            delete response?.req?.rawHeaders[3];
+            delete response?.req?.headers.cookie;
+            await this.userService.removeRefreshToken(phone);
+            const responseMessage: string = "خروج کاربر با موفقیت انجام شد"
+            return response
+                .status(HttpStatus.OK)
+                .json({
+                    message: responseMessage,
+                    statusCode: HttpStatus.OK
+                })
+        } catch (error) {
+            if (error instanceof HttpException) {
+                throw error;
+            } else {
+                throw new HttpException(
+                    (error),
+                    HttpStatus.INTERNAL_SERVER_ERROR
+                );
             }
-        );
-        response.clearCookie(
-            "access-token",
-            {
-                sameSite: 'none',
-                httpOnly: false,
-                secure: true
-            }
-        );
-        delete response?.req?.user;
-        delete response?.req?.cookies;
-        delete response?.req?.rawHeaders[3];
-        delete response?.req?.headers.cookie;
-        await this.userService.removeRefreshToken(phone);
-        const responseMessage: string = "خروج کاربر با موفقیت انجام شد"
-        return response
-            .status(HttpStatus.OK)
-            .json({
-                message: responseMessage,
-                statusCode: HttpStatus.OK
-            })
+        }
     }
 
-    async resendCode
-        (response: Response,
-            { phone }: ResendCodeDto
-        ): Promise<Response> {
-        const {
-            otp: { expireIn: otpExpireIn }
-        } = await this.userService.findUser(phone);
-        const now: number = new Date().getTime();
-        if (now < otpExpireIn) {
-            throw new HttpException(
-                "مدت زمان ارسال کد مجدد ۲ دقیقه می باشد", HttpStatus.BAD_REQUEST
-            )
-        }
-        const otpCode: number = generateOtpCode();
-        const text = `ترخینه
+    async resendCode(
+        response: Response,
+        { phone }: ResendCodeDto
+    ): Promise<Response> {
+        try {
+            const {
+                otp: { expireIn: otpExpireIn }
+            } = await this.userService.findUser(phone);
+            const now: number = new Date().getTime();
+            if (now < otpExpireIn) {
+                throw new HttpException(
+                    "مدت زمان ارسال کد مجدد ۲ دقیقه می باشد", HttpStatus.BAD_REQUEST
+                )
+            }
+            const otpCode: number = generateOtpCode();
+            const text = `ترخینه
             ارسال مجدد : ${otpCode}
             `;
-        SmsPanel(phone, otpCode, text);
-        return response
-            .status(HttpStatus.OK)
-            .json({
-                message: "کد با موفقیت ارسال شد",
-                statusCode: HttpStatus.OK
-            })
+            SmsPanel(phone, otpCode, text);
+            return response
+                .status(HttpStatus.OK)
+                .json({
+                    message: "کد با موفقیت ارسال شد",
+                    statusCode: HttpStatus.OK
+                })
+        } catch (error) {
+            if (error instanceof HttpException) {
+                throw error;
+            } else {
+                throw new HttpException(
+                    (error),
+                    HttpStatus.INTERNAL_SERVER_ERROR
+                );
+            }
+        }
     }
 
     async getTokens(
         phone: string, userId: string
     ): Promise<Token> {
-        const jwtPayload: JwtPayload = {
-            sub: userId,
-            phone
-        }
-        const [
-            accessToken,
-            refreshToken
-        ] = await Promise.all([
-            this.jwtService.signAsync(
-                jwtPayload, {
-                secret: this.configService.get<string>("JWT_ACCESS_TOKEN_SECRET"),
-                expiresIn: '1h'
-            }),
-            this.jwtService.signAsync(
-                jwtPayload, {
-                secret: this.configService.get<string>("JWT_REFRESH_TOKEN_SECRET"),
-                expiresIn: '3d'
-            })
-        ])
-        return {
-            accessToken,
-            refreshToken
+        try {
+            const jwtPayload: JwtPayload = {
+                sub: userId,
+                phone
+            }
+            const [
+                accessToken,
+                refreshToken
+            ] = await Promise.all([
+                this.jwtService.signAsync(
+                    jwtPayload, {
+                    secret: this.configService.get<string>("JWT_ACCESS_TOKEN_SECRET"),
+                    expiresIn: '1h'
+                }),
+                this.jwtService.signAsync(
+                    jwtPayload, {
+                    secret: this.configService.get<string>("JWT_REFRESH_TOKEN_SECRET"),
+                    expiresIn: '3d'
+                })
+            ])
+            return {
+                accessToken,
+                refreshToken
+            }
+        } catch (error) {
+            if (error instanceof HttpException) {
+                throw error;
+            } else {
+                throw new HttpException(
+                    (error),
+                    HttpStatus.INTERNAL_SERVER_ERROR
+                );
+            }
         }
     }
 
@@ -257,45 +316,67 @@ export class AuthService {
         refreshToken: string,
         userPhone: string
     ): Promise<string> {
-        const {
-            hashRT,
-            phone
-        } = await this.userService.findUser(userPhone);
-        const isTokensEqual: boolean = await bcrypt.compare(
-            refreshToken,
-            hashRT
-        );
-        if (!isTokensEqual) {
-            throw new HttpException(
-                "توکن نا معتبر ", HttpStatus.UNAUTHORIZED
+        try {
+            const {
+                hashRT,
+                phone
+            } = await this.userService.findUser(userPhone);
+            const isTokensEqual: boolean = await bcrypt.compare(
+                refreshToken,
+                hashRT
             );
-        }
-        const isTokenValid = await this.jwtService.verify(
-            refreshToken,
-            {
-                secret: this.configService.get<string>("JWT_REFRESH_TOKEN_SECRET")
+            if (!isTokensEqual) {
+                throw new HttpException(
+                    "توکن نا معتبر ", HttpStatus.UNAUTHORIZED
+                );
             }
-        )
-        if (!isTokenValid) {
-            throw new HttpException(
-                "توکن نا معتبر ", HttpStatus.UNAUTHORIZED
-            );
+            const isTokenValid = await this.jwtService.verify(
+                refreshToken,
+                {
+                    secret: this.configService.get<string>("JWT_REFRESH_TOKEN_SECRET")
+                }
+            )
+            if (!isTokenValid) {
+                throw new HttpException(
+                    "توکن نا معتبر ", HttpStatus.UNAUTHORIZED
+                );
+            }
+            return phone;
+        } catch (error) {
+            if (error instanceof HttpException) {
+                throw error;
+            } else {
+                throw new HttpException(
+                    (error),
+                    HttpStatus.INTERNAL_SERVER_ERROR
+                );
+            }
         }
-        return phone;
     }
 
     async getAccessToken(
         phone: string, userId: string
-    ) {
-        const jwtPayload: JwtPayload = {
-            sub: userId,
-            phone
+    ): Promise<string> {
+        try {
+            const jwtPayload: JwtPayload = {
+                sub: userId,
+                phone
+            }
+            const accessToken = await this.jwtService.signAsync(
+                jwtPayload, {
+                secret: this.configService.get<string>("JWT_ACCESS_TOKEN_SECRET"),
+                expiresIn: '1h'
+            })
+            return accessToken
+        } catch (error) {
+            if (error instanceof HttpException) {
+                throw error;
+            } else {
+                throw new HttpException(
+                    (error),
+                    HttpStatus.INTERNAL_SERVER_ERROR
+                );
+            }
         }
-        const accessToken = await this.jwtService.signAsync(
-            jwtPayload, {
-            secret: this.configService.get<string>("JWT_ACCESS_TOKEN_SECRET"),
-            expiresIn: '1h'
-        })
-        return accessToken
     }
 }
