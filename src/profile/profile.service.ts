9606eee961e86bb43cfb03ac6f6a475352c3b424
sadra soleimani <sadrasadrasadra6@gmail.com>
2024-04-24 15:39:16 +0330

    refactor: try catch handler <profile service>

--- src/profile/profile.service.ts
+++ src/profile/profile.service.ts
@@ -21,34 +21,56 @@ export class ProfileService {
         phone: string,
         response: Response
     ): Promise<Response> {
-        await this.userService.checkUsernameExists(
-            updateUserDto.username,
-            phone
-        )
-        deleteInvalidValue(updateUserDto);
-        await this.userService.updateUser(
-            updateUserDto,
-            phone
-        )
-        return response
-            .status(HttpStatus.CREATED)
-            .json({
-                message: "کاربر با موفقیت به روز رسانی  شد",
-                statusCode: HttpStatus.CREATED
-            })
+        try {
+            await this.userService.checkUsernameExists(
+                updateUserDto.username,
+                phone
+            )
+            deleteInvalidValue(updateUserDto);
+            await this.userService.updateUser(
+                updateUserDto,
+                phone
+            )
+            return response
+                .status(HttpStatus.CREATED)
+                .json({
+                    message: "کاربر با موفقیت به روز رسانی  شد",
+                    statusCode: HttpStatus.CREATED
+                })
+        } catch (error) {
+            if (error instanceof HttpException) {
+                throw error;
+            } else {
+                throw new HttpException(
+                    (error),
+                    HttpStatus.INTERNAL_SERVER_ERROR
+                );
+            }
+        }
     }
 
     async deleteUser(
         deleteUserDto: DeleteUserDto,
         response: Response
     ): Promise<Response> {
-        await this.userService.deleteUser(deleteUserDto)
-        return response
-            .status(HttpStatus.CREATED)
-            .json({
-                message: "کاربر با موفقیت حذف شد",
-                statusCode: HttpStatus.OK
-            })
+        try {
+            await this.userService.deleteUser(deleteUserDto)
+            return response
+                .status(HttpStatus.CREATED)
+                .json({
+                    message: "کاربر با موفقیت حذف شد",
+                    statusCode: HttpStatus.OK
+                })
+        } catch (error) {
+            if (error instanceof HttpException) {
+                throw error;
+            } else {
+                throw new HttpException(
+                    (error),
+                    HttpStatus.INTERNAL_SERVER_ERROR
+                );
+            }
+        }
     }
 
     async getUserOrders(
@@ -56,31 +78,53 @@ export class ProfileService {
         filterQuery: string,
         response: Response
     ): Promise<Response> {
-        const userOrders = await this.orderService.getUserOrders(
-            phone,
-            filterQuery
-        )
-        return response
-            .status(HttpStatus.OK)
-            .json({
-                userOrders,
-                statusCode: HttpStatus.OK
-            })
+        try {
+            const userOrders = await this.orderService.getUserOrders(
+                phone,
+                filterQuery
+            )
+            return response
+                .status(HttpStatus.OK)
+                .json({
+                    userOrders,
+                    statusCode: HttpStatus.OK
+                })
+        } catch (error) {
+            if (error instanceof HttpException) {
+                throw error;
+            } else {
+                throw new HttpException(
+                    (error),
+                    HttpStatus.INTERNAL_SERVER_ERROR
+                );
+            }
+        }
     }
 
     async cancelOrder(
         orderId: string,
         response: Response
     ): Promise<Response> {
-        await this.orderService.cancelOrder(
-            orderId
-        )
-        return response
-            .status(HttpStatus.OK)
-            .json({
-                message: "سفارش با موفقیت لغو شد",
-                statusCode: HttpStatus.OK
-            })
+        try {
+            await this.orderService.cancelOrder(
+                orderId
+            )
+            return response
+                .status(HttpStatus.OK)
+                .json({
+                    message: "سفارش با موفقیت لغو شد",
+                    statusCode: HttpStatus.OK
+                })
+        } catch (error) {
+            if (error instanceof HttpException) {
+                throw error;
+            } else {
+                throw new HttpException(
+                    (error),
+                    HttpStatus.INTERNAL_SERVER_ERROR
+                );
+            }
+        }
     }
 
     async createAddress(
@@ -88,16 +132,27 @@ export class ProfileService {
         createAddressDto: CreateAddressDto,
         response: Response
     ): Promise<Response> {
-        await this.userService.createAddress(
-            phone,
-            createAddressDto
-        );
-        return response
-            .status(HttpStatus.CREATED)
-            .json({
-                message: "ادرس با موفقیت ثبت شد",
-                statusCode: HttpStatus.CREATED
-            })
+        try {
+            await this.userService.createAddress(
+                phone,
+                createAddressDto
+            );
+            return response
+                .status(HttpStatus.CREATED)
+                .json({
+                    message: "ادرس با موفقیت ثبت شد",
+                    statusCode: HttpStatus.CREATED
+                })
+        } catch (error) {
+            if (error instanceof HttpException) {
+                throw error;
+            } else {
+                throw new HttpException(
+                    (error),
+                    HttpStatus.INTERNAL_SERVER_ERROR
+                );
+            }
+        }
     }
 
     async updateAddress(
@@ -105,29 +160,51 @@ export class ProfileService {
         response: Response,
         updateAddressDto: UpdateAddressDto,
     ): Promise<Response> {
-        await this.userService.updateAddress(
-            addressId,
-            updateAddressDto,
-        );
-        return response
-            .status(HttpStatus.OK)
-            .json({
-                message: "ادرس با موفقیت به روز  شد",
-                statusCode: HttpStatus.OK
-            })
+        try {
+            await this.userService.updateAddress(
+                addressId,
+                updateAddressDto,
+            );
+            return response
+                .status(HttpStatus.OK)
+                .json({
+                    message: "ادرس با موفقیت به روز  شد",
+                    statusCode: HttpStatus.OK
+                })
+        } catch (error) {
+            if (error instanceof HttpException) {
+                throw error;
+            } else {
+                throw new HttpException(
+                    (error),
+                    HttpStatus.INTERNAL_SERVER_ERROR
+                );
+            }
+        }
     }
 
     async deleteAddress(
         addressId: string,
         response: Response
     ): Promise<Response> {
-        await this.userService.deleteAddress(addressId);
-        return response
-            .status(HttpStatus.OK)
-            .json({
-                message: "ادرس با موفقیت حذف  شد",
-                statusCode: HttpStatus.OK
-            })
+        try {
+            await this.userService.deleteAddress(addressId);
+            return response
+                .status(HttpStatus.OK)
+                .json({
+                    message: "ادرس با موفقیت حذف  شد",
+                    statusCode: HttpStatus.OK
+                })
+        } catch (error) {
+            if (error instanceof HttpException) {
+                throw error;
+            } else {
+                throw new HttpException(
+                    (error),
+                    HttpStatus.INTERNAL_SERVER_ERROR
+                );
+            }
+        }
     }
 
     async getAddress(
@@ -137,53 +214,64 @@ export class ProfileService {
         limit: number,
         response: Response
     ): Promise<Response> {
-        const [
-            maxPage,
-            userAddresses
-        ] = await this.userService.getAddress(
-            phone,
-            page,
-            limit
-        );
-        const addresses = userAddresses.map(
-            (address) => {
-                if (!address.ownReceiver) {
-                    let {
-                        addressTitle,
-                        description,
-                        phone,
-                        name
-                    } = address.anotherReceiver;
-                    return {
-                        _id: address._id,
-                        addressTitle,
-                        description,
-                        phone, name,
-                        ownReceiver: address.ownReceiver
+        try {
+            const [
+                maxPage,
+                userAddresses
+            ] = await this.userService.getAddress(
+                phone,
+                page,
+                limit
+            );
+            const addresses = userAddresses.map(
+                (address) => {
+                    if (!address.ownReceiver) {
+                        let {
+                            addressTitle,
+                            description,
+                            phone,
+                            name
+                        } = address.anotherReceiver;
+                        return {
+                            _id: address._id,
+                            addressTitle,
+                            description,
+                            phone, name,
+                            ownReceiver: address.ownReceiver
+                        }
+                    } else {
+                        const {
+                            addressTitle,
+                            description,
+                            ownReceiver,
+                            _id,
+                        } = address;
+                        return {
+                            _id,
+                            phone,
+                            name: username,
+                            addressTitle,
+                            description,
+                            ownReceiver
+                        }
                     }
-                } else {
-                    const {
-                        addressTitle,
-                        description,
-                        ownReceiver,
-                        _id,
-                    } = address;
-                    return {
-                        _id,
-                        phone,
-                        name: username,
-                        addressTitle,
-                        description,
-                        ownReceiver
-                    }
-                }
-            })
-        return response
-            .status(HttpStatus.OK)
-            .json({
-                data: addresses,
-                maxPage
-            })
+                })
+            return response
+                .status(HttpStatus.OK)
+                .json({
+                    data: addresses,
+                    maxPage
+                })
+        } catch (error) {
+            if (error instanceof HttpException) {
+                throw error;
+            } else {
+                throw new HttpException(
+                    (error),
+                    HttpStatus.INTERNAL_SERVER_ERROR
+                );
+            }
+        }
     }
 
     async updateImage(
@@ -191,65 +279,87 @@ export class ProfileService {
         file: Express.Multer.File,
         response: Response
     ): Promise<Response> {
-        const imageUrl = this.storageService.getFileLink(
-            file.filename,
-            USER_FOLDER
-        )
-        const storageQuery = this.storageService.uploadSingleFile(
-            file.filename,
-            file.buffer,
-            USER_FOLDER
-        )
-        const userQuery = this.userService.updateImage(
-            phone,
-            file.filename,
-            imageUrl
-        )
-        await Promise.all([
-            storageQuery,
-            userQuery
-        ])
-        return response
-            .status(HttpStatus.OK)
-            .json({
-                message: "بروفایل کاربر با موفقیت به روز رسانی شد",
-                statusCode: HttpStatus.OK
-            })
+        try {
+            const imageUrl = this.storageService.getFileLink(
+                file.filename,
+                USER_FOLDER
+            )
+            const storageQuery = this.storageService.uploadSingleFile(
+                file.filename,
+                file.buffer,
+                USER_FOLDER
+            )
+            const userQuery = this.userService.updateImage(
+                phone,
+                file.filename,
+                imageUrl
+            )
+            await Promise.all([
+                storageQuery,
+                userQuery
+            ])
+            return response
+                .status(HttpStatus.OK)
+                .json({
+                    message: "بروفایل کاربر با موفقیت به روز رسانی شد",
+                    statusCode: HttpStatus.OK
+                })
+        } catch (error) {
+            if (error instanceof HttpException) {
+                throw error;
+            } else {
+                throw new HttpException(
+                    (error),
+                    HttpStatus.INTERNAL_SERVER_ERROR
+                );
+            }
+        }
     }
 
     async deleteImage(
         phone: string,
         response: Response
     ): Promise<Response> {
-        const {
-            imageUrl,
-            image
-        } = await this.userService.findUser(
-            phone
-        );
-        if (!image) {
-            throw new HttpException(
-                "کاربر فاقد عکس میباشد",
-                HttpStatus.BAD_REQUEST
+        try {
+            const {
+                imageUrl,
+                image
+            } = await this.userService.findUser(
+                phone
+            );
+            if (!image) {
+                throw new HttpException(
+                    "کاربر فاقد عکس میباشد",
+                    HttpStatus.BAD_REQUEST
+                )
+            }
+            const storageQuery = this.storageService.deleteFile(
+                image,
+                USER_FOLDER
             )
+            const userQuery = this.userService.deleteImage(
+                phone
+            )
+            await Promise.all([
+                storageQuery,
+                userQuery
+            ])
+            return response
+                .status(HttpStatus.OK)
+                .json({
+                    message: "بروفایل کاربر با موفقیت به حذف شد",
+                    statusCode: HttpStatus.OK
+                })
+        } catch (error) {
+            if (error instanceof HttpException) {
+                throw error;
+            } else {
+                throw new HttpException(
+                    (error),
+                    HttpStatus.INTERNAL_SERVER_ERROR
+                );
+            }
         }
-        const storageQuery = this.storageService.deleteFile(
-            image,
-            USER_FOLDER
-        )
-        const userQuery = this.userService.deleteImage(
-            phone
-        )
-        await Promise.all([
-            storageQuery,
-            userQuery
-        ])
-        return response
-            .status(HttpStatus.OK)
-            .json({
-                message: "بروفایل کاربر با موفقیت به حذف شد",
-                statusCode: HttpStatus.OK
-            })
     }
 
     async addFavoriteFood(
@@ -257,13 +367,24 @@ export class ProfileService {
         foodId: string,
         response: Response
     ): Promise<Response> {
-        await this.userService.addFavoriteFood(phone, foodId);
-        return response
-            .status(HttpStatus.OK)
-            .json({
-                message: "غذا به لیست مورد پسند اضافه شد",
-                statusCode: HttpStatus.OK
-            })
+        try {
+            await this.userService.addFavoriteFood(phone, foodId);
+            return response
+                .status(HttpStatus.OK)
+                .json({
+                    message: "غذا به لیست مورد پسند اضافه شد",
+                    statusCode: HttpStatus.OK
+                })
+        } catch (error) {
+            if (error instanceof HttpException) {
+                throw error;
+            } else {
+                throw new HttpException(
+                    (error),
+                    HttpStatus.INTERNAL_SERVER_ERROR
+                );
+            }
+        }
     }
 
     async removeFavoriteFood(
@@ -271,16 +392,27 @@ export class ProfileService {
         foodId: string,
         response: Response
     ): Promise<Response> {
-        await this.userService.removeFavoriteFood(
-            phone,
-            foodId
-        );
-        return response
-            .status(HttpStatus.OK)
-            .json({
-                message: "غذا از لیست مورد پسند حذف شد",
-                statusCode: HttpStatus.OK
-            })
+        try {
+            await this.userService.removeFavoriteFood(
+                phone,
+                foodId
+            );
+            return response
+                .status(HttpStatus.OK)
+                .json({
+                    message: "غذا از لیست مورد پسند حذف شد",
+                    statusCode: HttpStatus.OK
+                })
+        } catch (error) {
+            if (error instanceof HttpException) {
+                throw error;
+            } else {
+                throw new HttpException(
+                    (error),
+                    HttpStatus.INTERNAL_SERVER_ERROR
+                );
+            }
+        }
     }
 
     async getFavoriteFood(
@@ -291,30 +423,41 @@ export class ProfileService {
         query: string,
         response: Response
     ): Promise<Response> {
-        const favoriteFoodId = await this.userService.getFavoriteFoodId(
-            phone
-        );
-        let favoriteFood = await this.foodService.getFavoriteFood(
-            favoriteFoodId,
-            mainCategory,
-            page,
-            limit,
-            query
-        )
-        const maxPage = Math.ceil(
-            favoriteFood?.length / limit
-        )
-        favoriteFood = pagination(
-            favoriteFood,
-            limit,
-            page
-        );
-        return response
-            .status(HttpStatus.OK)
-            .json({
+        try {
+            const favoriteFoodId = await this.userService.getFavoriteFoodId(
+                phone
+            );
+            let favoriteFood = await this.foodService.getFavoriteFood(
+                favoriteFoodId,
+                mainCategory,
+                page,
+                limit,
+                query
+            )
+            const maxPage = Math.ceil(
+                favoriteFood?.length / limit
+            )
+            favoriteFood = pagination(
                 favoriteFood,
-                maxPage,
-                statusCode: HttpStatus.OK
-            })
+                limit,
+                page
+            );
+            return response
+                .status(HttpStatus.OK)
+                .json({
+                    favoriteFood,
+                    maxPage,
+                    statusCode: HttpStatus.OK
+                })
+        } catch (error) {
+            if (error instanceof HttpException) {
+                throw error;
+            } else {
+                throw new HttpException(
+                    (error),
+                    HttpStatus.INTERNAL_SERVER_ERROR
+                );
+            }
+        }
     }
 }
