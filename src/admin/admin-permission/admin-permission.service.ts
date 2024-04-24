b56f8e482bdc61d9437d185bc729916d0b68a465
sadra soleimani <sadrasadrasadra6@gmail.com>
2024-04-24 12:32:45 +0330

    refactor: try catch handler <admin permission service>

--- src/admin/admin-permission/admin-permission.service.ts
+++ src/admin/admin-permission/admin-permission.service.ts
@@ -1,4 +1,4 @@
-import { HttpStatus, Injectable, forwardRef, Inject } from '@nestjs/common';
+import { HttpStatus, Injectable, forwardRef, Inject, HttpException } from '@nestjs/common';
 import { UserService } from 'src/user/user.service';
 import { GiveAccessDto } from './dto/giveAccess.dto';
 import { Response } from 'express';
@@ -14,31 +14,51 @@ export class AdminPermissionService {
         giveAccessDto: GiveAccessDto,
         response: Response
     ): Promise<Response> {
-        await this.userService.giveAdminAccess(
-            giveAccessDto.phone
-        )
-        return response
-            .status(HttpStatus.OK)
-            .json({
-                message: "دسترسی ادمین به کاربر داده شد",
-                statusCode: HttpStatus.OK
-            })
+        try {
+            await this.userService.giveAdminAccess(
+                giveAccessDto.phone
+            )
+            return response
+                .status(HttpStatus.OK)
+                .json({
+                    message: "دسترسی ادمین به کاربر داده شد",
+                    statusCode: HttpStatus.OK
+                })
+        } catch (error) {
+            if (error instanceof HttpException) {
+                throw error;
+            } else {
+                throw new HttpException(
+                    (error),
+                    HttpStatus.INTERNAL_SERVER_ERROR
+                );
+            }
+        }
     }
 
     async giveUserAccess(
         giveAccessDto: GiveAccessDto,
         response: Response
     ): Promise<Response> {
-        await this.userService.giveUserAccess(
-            giveAccessDto.phone
-        )
-        ////////////
-        ///////////
-        return response
-            .status(HttpStatus.OK)
-            .json({
-                message: "دسترسی عادی به کاربر داده شد",
-                statusCode: HttpStatus.OK
-            })
+        try {
+            await this.userService.giveUserAccess(
+                giveAccessDto.phone
+            )
+            return response
+                .status(HttpStatus.OK)
+                .json({
+                    message: "دسترسی عادی به کاربر داده شد",
+                    statusCode: HttpStatus.OK
+                })
+        } catch (error) {
+            if (error instanceof HttpException) {
+                throw error;
+            } else {
+                throw new HttpException(
+                    (error),
+                    HttpStatus.INTERNAL_SERVER_ERROR
+                );
+            }
+        }
     }
 }
