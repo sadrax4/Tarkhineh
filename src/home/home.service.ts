d34db3af9f93c1a179e45cbe3a8dbde5eb8ec85d
sadra soleimani <sadrasadrasadra6@gmail.com>
2024-04-24 15:10:06 +0330

    refactor: try catch handler <home service>

--- src/home/home.service.ts
+++ src/home/home.service.ts
@@ -1,4 +1,4 @@
-import { HttpStatus, Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { Response } from 'express';
 import { FoodService } from 'src/food/food.service';
 import { SearchDto } from './dto';
@@ -14,12 +14,23 @@ export class HomeService {
         { search }: SearchDto,
         response: Response
     ): Promise<Response> {
-        const foods = await this.foodService.homeSearchFood(search)
-        return response
-            .status(HttpStatus.OK)
-            .json({
-                foods,
-                statusCode: HttpStatus.OK
-            })
+        try {
+            const foods = await this.foodService.homeSearchFood(search)
+            return response
+                .status(HttpStatus.OK)
+                .json({
+                    foods,
+                    statusCode: HttpStatus.OK
+                })
+        } catch (error) {
+            if (error instanceof HttpException) {
+                throw error;
+            } else {
+                throw new HttpException(
+                    (error),
+                    HttpStatus.INTERNAL_SERVER_ERROR
+                );
+            }
+        }
     }
 }
